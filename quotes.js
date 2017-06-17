37cc0b985ab2a490cc084942143c78f61d23d41b
Miguel Ángel Moyá <miguel.moya.n@gmail.com>
2017-06-17 10:21:47 +0200

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,24 @@
+    var quotes = [
+        {
+            text: "“eExisten tres posibles respuestas ante un diseño: 'Sí', 'No' y '¡Wow!'. Hay que aspirar a Wow.”",
+			author: "Milton Glaser",
+			img: "img/backgrounds/milton_glaser.jpg",
+		},
+		{
+			text: "“eEl diseño es el embajador silencioso de tu marca.”",
+			author: "Paul Rand",
+			img: "img/backgrounds/paul_rand.jpg",
+		},
+		{
+			text: "“eUn buen diseño puede soportar la moda de 10 años.”",
+			author: "Yves Saint Laurent",
+			img: "img/backgrounds/yves_saint_laurent.jpg",
+		},
+        {
+			text: "“eSi no es una marca es una mercancía.”",
+			author: "Philip Kotler",
+			img: "img/backgrounds/philip_kotler.jpg",
+		}
+    ];
+var quote = quotes[Math.floor(Math.random() * quotes.length)];
+var image = quote.img;
